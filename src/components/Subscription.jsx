3bdd09cd911772ef80ed25b8dded4570b33190d0
bijendra-putul <bijendra3putul@yahoo.co.in>
2026-03-20 22:49:28 +0530

    test mail check

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,17 +1,27 @@
 export default function Subscription() {
 
   const handlePayment = () => {
-    const options = {
+       const options = {
       key: "rzp_test_STXzPHIbayB1EE,861gtKdWTPTDcfXzkN2SFvCa",
       amount: 499,
       currency: "INR",
       name: "IPL Predictor",
+      description: "Premium Subscription",
 
       handler: function () {
         alert("Payment Successful!");
         localStorage.setItem("premium", "true");
         window.location.reload();
       },
+      prefill: {
+      name: "User",
+      email: "test@test.com",
+      contact: "9999999999",
+    },
+
+    theme: {
+      color: "#22c55e",
+    },
     };
 
     const rzp = new window.Razorpay(options);
