2583aaba64b6adf36b6544ef3bcc7bd268776a96
bijendra-putul <bijendra3putul@yahoo.co.in>
2026-03-21 11:38:34 +0530

    add link button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <h1 className="logo">🏏 IPL Predictor</h1>
+      <a href="https://inr.deals/3ikJLT" target="_blank">
       <button className="btn btn-primary">Go Premium</button>
+    </a>
     </div>
   );
 }
